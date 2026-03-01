96c8db78187e361e7d8cb0e32b80b265243997ff
AzamatAbraev <azamatabraev07@gmail.com>
2026-03-01 19:04:24 +0500

    admin.py fixed for testing the change

--- discussion_forum_00016438/forum/admin.py
+++ discussion_forum_00016438/forum/admin.py
@@ -1,28 +1,31 @@
 from django.contrib import admin
 from .models import Topic, Post, Comment, Tag
 
+
 @admin.register(Topic)
 class TopicAdmin(admin.ModelAdmin):
-  list_display = ("name", "created_at")
-  search_fields = ("name", "description")
+    list_display = ("name", "created_at")
+    search_fields = ("name", "description")
+
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-  list_display = ("title", "topic", "author", "created_at")
-  search_fields = ("title", "body")
-  list_filter = ("created_at", "topic", "tags")
+    list_display = ("title", "topic", "author", "created_at")
+    search_fields = ("title", "body")
+    list_filter = ("created_at", "topic", "tags")
   
-  prepopulated_fields = {"slug": ("title",)}
+    prepopulated_fields = {"slug": ("title",)}
   
-  filter_horizontal = ("tags",)
+    filter_horizontal = ("tags",)
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-  list_display = ("author", "post", "created_at", "body_snippet")
-  list_filter = ("created_at", "author")
+    list_display = ("author", "post", "created_at", "body_snippet")
+    list_filter = ("created_at", "author")
   
-  def body_snippet(self, obj):
-    return obj.body[:50] + "..." if len(obj.body) > 50 else obj.body
-  body_snippet.short_description = "Comment Preview"
+    def body_snippet(self, obj):
+        return obj.body[:50] + "..." if len(obj.body) > 50 else obj.body
+    body_snippet.short_description = "Comment Preview"
 
-admin.site.register(Tag)
+    admin.site.register(Tag)
