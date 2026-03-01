7c46d22bd1e3cb0c3ebfda2dfe8cb0e48d37003c
AzamatAbraev <azamatabraev07@gmail.com>
2026-03-01 19:49:12 +0500

    env difference issue fix

--- discussion_forum_00016438/config/settings.py
+++ discussion_forum_00016438/config/settings.py
@@ -23,7 +23,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY","django-insecure-fallback-key-for-dev-only")
 # if not SECRET_KEY:
 #     raise ValueError("DJANGO_SECRET_KEY environment variable is required")
 
