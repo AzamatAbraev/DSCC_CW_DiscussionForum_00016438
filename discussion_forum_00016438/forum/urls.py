7a4326f091935abb257c80d171db7ae61d2062fa
AzamatAbraev <azamatabraev07@gmail.com>
2026-03-01 19:33:19 +0500

    lint fix; change of rule 2

--- discussion_forum_00016438/forum/urls.py
+++ discussion_forum_00016438/forum/urls.py
@@ -4,12 +4,12 @@ from django.contrib.auth import views as auth_views
 from .views import SignUpView
 
 urlpatterns = [
-  path('', views.PostListView.as_view(), name='post_list'),
-  path('post/new/', views.PostCreateView.as_view(), name='post_create'),
-  path('post/<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
-  path('post/<slug:slug>/update/', views.PostUpdateView.as_view(), name='post_update'),
-  path('post/<slug:slug>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
-  path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-  path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-  path('signup/', SignUpView.as_view(), name='signup'),
+	path('', views.PostListView.as_view(), name='post_list'),
+	path('post/new/', views.PostCreateView.as_view(), name='post_create'),
+	path('post/<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
+	path('post/<slug:slug>/update/', views.PostUpdateView.as_view(), name='post_update'),
+	path('post/<slug:slug>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+	path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+	path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+	path('signup/', SignUpView.as_view(), name='signup'),
 ]
